56f786686e85c2769c932ab0f6e8eb3ebeae820b
Vova Ragoulin <vovaragulin@gmail.com>
2020-08-11 21:34:46 +0300

    Добавьте файлы проекта.

---
+++ LearningPython.py
@@ -0,0 +1,14 @@
+
+
+#Комментарий блочный
+print("Full name:", "Tom", "Smith") #Комментарий строчный
+name = input("Введите имя: ")
+print("Привет", name)
+
+
+#Переменные и типы данных
+#and, as, assert, break, class, continue, def, del, elif, else, except, False, finally, 
+#for, from, global, if, import, in, is, lambda, None, nonlocal, not, or, pass, raise, 
+#return, True, try, while, with, yield
+
+
